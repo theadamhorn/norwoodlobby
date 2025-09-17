315b9c356c01129f9eed2c805a6f53bea7f2b04d
Adam Horn <mr.amhorn@gmail.com>
2025-09-17 12:42:20 -0700

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+  base: "/theadamhorn/",   // 👈 important for Pages
+  build: {
+    target: "esnext",
+  },
   server: {
     port: 3000,
-  },
-  base: '/',  
-  build: {
-    target: 'esnext',
   },
 });
