edcade1a495a161e4258aa1d190951768e5b5d87
Adam Horn <mr.amhorn@gmail.com>
2025-09-17 13:43:28 -0700

    changing build in config to norwoodlobby

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "~": path.resolve(__dirname, "src"),
     },
   },
-  base: "/theadamhorn/", 
+  base: "/norwoodlobby/", 
   build: {
     target: "esnext",
   },
